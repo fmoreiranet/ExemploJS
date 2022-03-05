b95da834e9138bc35feab69757d62253d3206090
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-03-04 21:55:45 -0300

    2022-03-04

--- app/modules/produto/produto.js
+++ app/modules/produto/produto.js
@@ -9,16 +9,29 @@ class Produto {
             this.foto3 = ""
     };
 
+    //Create Read Update Delete (CRUD)
     //Adicionar produto ao armazenamento
     add(produto) {
-        produto = JSON.stringify(produto); //JSON.parse() -> pega um json e converte em objeto
-        localStorage.setItem('produtos', produto);
-        return true;
+        try {
+            var baseProduto = [];  //Criando Vetor para receber os dados do localstorage
+            baseProduto = JSON.parse(localStorage.getItem('produtos'));//JSON.parse() -> pega um json e converte em objeto
+            baseProduto.push(produto); // Adicionando o produto na lista de produtos;
+
+            var produtosJson = JSON.stringify(baseProduto); //Criando JSON dos objetos na baseProduto
+            localStorage.setItem('produtos', produtosJson);
+            return true;
+        } catch (ex) {
+            console.error(ex);
+            return false;
+        }
     }
 
     //listar produtos
+    getAll() {
+        return JSON.parse(localStorage.getItem('produtos'));
+    }
 
-    //atualizr produtos
+    //atualizar produtos
 
     //remover produtos
 }
--- app/modules/produto/produtoController.js
+++ app/modules/produto/produtoController.js
@@ -1,14 +1,27 @@
 const produtoController = {
     produtos: [],
-    addProduto: function() {
+    addProduto: function () {
         var produto = new Produto;
         produto.nome = document.getElementById('nome').value;
         produto.descricao = document.getElementById('descricao').value;
         produto.quant = parseInt(document.getElementById('quant').value);
         produto.valor = parseFloat(document.getElementById('valor').value);
-        this.produtos.push(produto);
-        if (produto.add(this.produtos)) {
+        //this.produtos.push(produto);
+        if (produto.add(produto)) {
             alert("Produto Adicionado!");
         }
+        this.getAllProduto();
+    },
+    getAllProduto: function () {
+        var produto = new Produto;
+        this.produtos = produto.getAll();
+        var tabela = "<table class='table'> <tr> <th>nome</th> <th>Desescricao</th> <th>Quant</th> <th>Valor</th> </tr>";
+
+        for (var index = 0; index < this.produtos.length; index++) {
+            tabela += "<tr> <td>" + this.produtos[index].nome + "</td> <td>" + this.produtos[index].descricao + "</td> <td>" + this.produtos[index].quant + "</td> <td> R$" + this.produtos[index].valor.toFixed(2) + "</td> </tr>";
+        }
+
+        tabela += "</table>";
+        document.getElementById("listaProdutos").innerHTML = tabela
     }
 }
