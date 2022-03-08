cfe60e2d67916810588aaff10f3e64df038c2244
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-03-07 21:42:56 -0300

    2022-03-07

--- app/modules/produto/produto.js
+++ app/modules/produto/produto.js
@@ -13,25 +13,57 @@ class Produto {
     //Adicionar produto ao armazenamento
     add(produto) {
         try {
-            var baseProduto = [];  //Criando Vetor para receber os dados do localstorage
-            baseProduto = JSON.parse(localStorage.getItem('produtos'));//JSON.parse() -> pega um json e converte em objeto
-            baseProduto.push(produto); // Adicionando o produto na lista de produtos;
+            this.validData();
+            var baseProduto = JSON.parse(localStorage.getItem('produtos'));//JSON.parse() -> pega um json e converte em objeto
+            if (baseProduto == null) {
+                baseProduto = [];  //Criando Vetor para receber os dados do localstorage
+            }
 
+            baseProduto.push(produto); // Adicionando o produto na lista de produtos;
             var produtosJson = JSON.stringify(baseProduto); //Criando JSON dos objetos na baseProduto
             localStorage.setItem('produtos', produtosJson);
-            return true;
+            //return true;
         } catch (ex) {
             console.error(ex);
-            return false;
+            throw ex;
+            //return false;
         }
     }
 
     //listar produtos
     getAll() {
-        return JSON.parse(localStorage.getItem('produtos'));
+        var baseProduto = JSON.parse(localStorage.getItem('produtos'));
+        if (baseProduto == null) {
+            baseProduto = [];  //Criando Vetor para receber os dados do localstorage
+        }
+        return baseProduto;
     }
 
     //atualizar produtos
 
     //remover produtos
+
+    //validarDados
+    validData() {
+        var erros = "";
+        if (!this.nome || this.nome == "") {
+            erros += "Nome em branco!\n";
+        }
+
+        if (!this.descricao || this.descricao == "") {
+            erros += "Descrição em branco!\n";
+        }
+
+        if (!this.quant || this.quant == 0) {
+            erros += "Quantidade não pode esta zerada!\n";
+        }
+
+        if (!this.valor || this.valor == 0) {
+            erros += "Valor não pode esta zerado!\n";
+        }
+
+        if (erros != "") {
+            throw erros;
+        }
+    }
 }
--- app/modules/produto/produtoController.js
+++ app/modules/produto/produtoController.js
@@ -1,21 +1,27 @@
 const produtoController = {
-    produtos: [],
+    produtos: [],//{} um objeto  e assim [] é um veto
     addProduto: function () {
-        var produto = new Produto;
-        produto.nome = document.getElementById('nome').value;
-        produto.descricao = document.getElementById('descricao').value;
-        produto.quant = parseInt(document.getElementById('quant').value);
-        produto.valor = parseFloat(document.getElementById('valor').value);
-        //this.produtos.push(produto);
-        if (produto.add(produto)) {
+        try {
+
+            var produto = new Produto;
+            produto.nome = document.getElementById('nome').value;
+            produto.descricao = document.getElementById('descricao').value;
+            produto.quant = parseInt(document.getElementById('quant').value);
+            produto.valor = parseFloat(document.getElementById('valor').value);
+            //this.produtos.push(produto);
+            //if (produto.add(produto)) {
+            produto.add(produto);
             alert("Produto Adicionado!");
+            //}
+            this.getAllProduto();
+        } catch (ex) {
+            alert(ex);
         }
-        this.getAllProduto();
     },
     getAllProduto: function () {
         var produto = new Produto;
         this.produtos = produto.getAll();
-        var tabela = "<table class='table'> <tr> <th>nome</th> <th>Desescricao</th> <th>Quant</th> <th>Valor</th> </tr>";
+        var tabela = "<table class='table'> <tr> <th>Nome</th> <th>Desescricao</th> <th>Quant</th> <th>Valor</th> </tr>";
 
         for (var index = 0; index < this.produtos.length; index++) {
             tabela += "<tr> <td>" + this.produtos[index].nome + "</td> <td>" + this.produtos[index].descricao + "</td> <td>" + this.produtos[index].quant + "</td> <td> R$" + this.produtos[index].valor.toFixed(2) + "</td> </tr>";
