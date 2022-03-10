6429d17c631c91ceb1b4d43beab1e9f4da8997a2
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-03-09 21:49:44 -0300

    2022-03-09

--- app/modules/produto/produtoController.js
+++ app/modules/produto/produtoController.js
@@ -8,6 +8,7 @@ const produtoController = {
             produto.descricao = document.getElementById('descricao').value;
             produto.quant = parseInt(document.getElementById('quant').value);
             produto.valor = parseFloat(document.getElementById('valor').value);
+            produto.foto1 = document.getElementById('foto').filename;
             //this.produtos.push(produto);
             //if (produto.add(produto)) {
             produto.add(produto);
@@ -21,10 +22,10 @@ const produtoController = {
     getAllProduto: function () {
         var produto = new Produto;
         this.produtos = produto.getAll();
-        var tabela = "<table class='table'> <tr> <th>Nome</th> <th>Desescricao</th> <th>Quant</th> <th>Valor</th> </tr>";
+        var tabela = "<table class='table'> <tr> <th>Nome</th> <th>Desescricao</th> <th>Quant</th> <th>Valor</th> <th>//</th> <th>X</th></tr>";
 
         for (var index = 0; index < this.produtos.length; index++) {
-            tabela += "<tr> <td>" + this.produtos[index].nome + "</td> <td>" + this.produtos[index].descricao + "</td> <td>" + this.produtos[index].quant + "</td> <td> R$" + this.produtos[index].valor.toFixed(2) + "</td> </tr>";
+            tabela += "<tr> <td>" + this.produtos[index].nome + "</td> <td>" + this.produtos[index].descricao + "</td> <td>" + this.produtos[index].quant + "</td> <td> R$" + this.produtos[index].valor.toFixed(2) + "</td> <td onclick='produtoController.getProduto(" + index + ")'>//</td> <td>X</td></tr>";
         }
 
         tabela += "</table>";
@@ -42,5 +43,25 @@ const produtoController = {
                 erros[index].style = "display: none";
             }
         }
+    },
+    validRequired2: function () {
+        var campos = document.getElementsByClassName("required"); //document.querySelectorAll(".required")
+        var erros = document.getElementsByClassName("textErro");
+        for (var index = 0; index < campos.length; index++) {
+            if (campos[index].value == "") {
+                erros[index].style = "display: block";
+            } else {
+                erros[index].style = "display: none";
+            }
+        }
+    },
+    getProduto: function (index = 0) {
+        console.log(this.produtos[index]);
+        var produto = this.produtos[index];
+        document.getElementById('nome').value = produto.nome;
+        document.getElementById('descricao').value = produto.descricao;
+        document.getElementById('quant').value = produto.quant;
+        document.getElementById('valor').value = produto.valor;
+        document.getElementById('foto').filename = produto.foto;
     }
 }
